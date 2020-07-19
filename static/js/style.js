40abb2867dbb1b9dc8ed6f33d2d0f65c362e90d8
NickBell123 <nick.bell2008@gmail.com>
2020-07-19 11:36:25 +0100

    used chartist.js to create pie chart overview of user holdings

--- static/js/style.js
+++ static/js/style.js
@@ -14,5 +14,40 @@ $(document).ready(function () {
             "orderable": false
         }]       
     });
+
+    let myAsset = []
+
+    chartAsset = $('.current_asset')
+    for(coin of chartAsset){
+        myAsset.push(coin.textContent)
+    }
+    console.log(chartAsset)
+    
+    let myData = []
+
+    chartData = $('.current_value')
+    for (value of chartData){
+        v = value.textContent
+        vnum = v.slice(1)
+        num = parseFloat(vnum)
+        myData.push(num)
+        console.log(typeof(num))
+    }
+
+    console.log(myData)
+
+    var data = {
+        // labels: myAsset,
+        series: myData
+      };
+      
+      var sum = function(a, b) { return a + b };
+      
+      new Chartist.Pie('.ct-chart', data, {
+        labelInterpolationFnc: function(value, idx) {
+          let percentage = Math.round(value / data.series.reduce(sum) * 100) + '%';
+          return myAsset[idx] + ' ' + percentage;
+        }
+      });
     
 });
