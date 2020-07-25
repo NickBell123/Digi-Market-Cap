7046a0f5714b94bd83742c5694a6340dbb5c4e58
NickBell123 <nick.bell2008@gmail.com>
2020-07-25 12:06:32 +0100

    wired up reg/login with mongodb. created update and push lines to add bagz to create a array in the user document in mongo.

--- app.py
+++ app.py
@@ -12,24 +12,27 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
-
+# Api call for individual cryptos
 r = requests.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=50&CMC_PRO_API_KEY=7d99530e-32dc-4fff-96ee-4b3811b660de')
 results = r.json()
 data = results['data']
-
+# Api call for global crypto stats
 r = requests.get('https://pro-api.coinmarketcap.com//v1/global-metrics/quotes/latest?&CMC_PRO_API_KEY=7d99530e-32dc-4fff-96ee-4b3811b660de')
 results = r.json()
 global_data = results['data']
 
-
+# Check for user session or display login
 @app.route('/')
 def index():
   if 'username' in session:
-    return 'You are logged in as '+ session['username']
+    
+    
+    
+    return redirect(url_for('coin_list', username = session['username']))
  
   return render_template("index.html")
 
-
+# check login values
 @app.route('/login', methods=["POST"])
 def login():
   user = mongo.db.user
@@ -38,10 +41,13 @@ def login():
   if login_user:
     if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_user['password']) == True:
       session['username'] = request.form['username']
-      return 'You are logged in as '+ session['username']
+      user = mongo.db.user.find_one({'name': session['username']})
+      return redirect(url_for('index', user=user))
     
   return'Inavaild username or password'    
 
+
+# Register for new users, check for vaild username (one which has not be taken)
 @app.route('/register', methods=["GET", "POST"])
 def register():
   if request.method == "POST":
@@ -50,45 +56,50 @@ def register():
     # if no matching username found
     if exsisting_user is None:
       hashpassword = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-      user.insert({'name': request.form['username'], 'password': hashpassword})
+      user.insert_one({'name': request.form['username'], 'password': hashpassword})
       session['username'] = request.form['username']
-      return redirect(url_for('login'))
+      return redirect(url_for('index'))
     return 'SORRY! That username is taken'
   return render_template("register.html")
 
-@app.route('/coin_list')
-def coin_list():
-   return render_template("coin_list.html", data=data, global_data=global_data)
+# Api individual coin stat route
+@app.route('/coin_list/<username>')
+def coin_list(username):
+  
+  return render_template("coin_list.html", data=data, global_data=global_data)
 
-
+#Create a purchase or holding of crypto
 @app.route('/create_a_bag')
 def create_a_bag():
   return render_template("create_a_bag.html", data=data)
 
+#Add the purchase/holding to my bags function
+@app.route('/add_to_bagz/<username>', methods=["POST"])
+def add_to_bagz(username):
+  user = mongo.db.user.update_one({'name': session['username']},
+  {'$push': {'positions': request.form.to_dict()}})
+  
+  return redirect(url_for('get_my_bagz', username = session['username']))
 
-@app.route('/add_to_bagz', methods=["POST"])
-def add_to_bagz():
-  users=mongo.db.users
-  users.insert_one(request.form.to_dict())
-  return redirect(url_for('get_my_bagz'))
+#display/read users puchases/holdings
+@app.route('/get_my_bagz/<username>')
+def get_my_bagz(username):
+  user = mongo.db.user.find_one({'name': session['username']})
+ 
+  return render_template('my_bagz.html', positions=user['positions'], data=data, username = session['username'])
 
-
-@app.route('/get_my_bagz')
-def get_my_bagz():
-    
-  return render_template("my_bagz.html", users=mongo.db.users.find(), data=data)
-
-
-@app.route('/edit_bag/<bag_id>' )
+#update users puchases/holdings
+@app.route('/edit_bag/<bag_id>')
 def edit_bag(bag_id):
   the_bag = mongo.db.users.find_one({"_id": ObjectId(bag_id)})
     
   return render_template("edit_bag.html", bag=the_bag, data=data)
 
-
+#update users puchases/holdings
 @app.route('/update_bag/<bag_id>', methods=["POST"])
 def update_bag(bag_id):
   bag = mongo.db.users
+  
   bag.update({'_id': ObjectId(bag_id)},
   {
     'assets': request.form.get('assets'),
@@ -98,7 +109,7 @@ def update_bag(bag_id):
   })
   return redirect(url_for('get_my_bagz'))
 
-
+#delete users puchases/holdings
 @app.route('/delete_bag/<bag_id>')
 def delete_bag(bag_id):
   mongo.db.users.delete_one({'_id': ObjectId(bag_id)})
