e8e433a5ec51355a1d11d9b44771134735ac7a94
NickBell123 <nick.bell2008@gmail.com>
2020-07-27 20:42:07 +0100

    re-add

--- app.py
+++ app.py
@@ -12,24 +12,23 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
-# Api call for individual cryptos
+"""1st Api call to CMC for list of crypto"""
 r = requests.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=50&CMC_PRO_API_KEY=' + os.environ.get('API_KEY'))
 results = r.json()
 data = results['data']
-# Api call for global crypto stats
+"""2nd Api call to CMC for Market Stats of crypto"""
 r = requests.get('https://pro-api.coinmarketcap.com//v1/global-metrics/quotes/latest?&CMC_PRO_API_KEY=' + os.environ.get('API_KEY'))
 results = r.json()
 global_data = results['data']
 
-# Check for user session or display login
+"""Check if user is logged in already Y - coin list N -signin/up"""
 @app.route('/')
 def index():
   if 'username' in session:
     return redirect(url_for('coin_list', username = session['username']))
-
   return render_template("index.html")
 
-# check login values
+"""Check Mongo for usernames & password match"""
 @app.route('/login', methods=["POST"])
 def login():
   user = mongo.db.user
@@ -43,14 +42,12 @@ def login():
     
   return'Inavaild username or password'    
 
-
-# Register for new users, check for vaild username (one which has not be taken)
+"""Reg page Check for available username. All usernames are unique"""
 @app.route('/register', methods=["GET", "POST"])
 def register():
   if request.method == "POST":
     user = mongo.db.user
     exsisting_user = user.find_one({'name': request.form['username']})
-    # if no matching username found
     if exsisting_user is None:
       hashpassword = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
       user.insert_one({'name': request.form['username'], 'password': hashpassword})
@@ -59,19 +56,20 @@ def register():
     return 'SORRY! That username is taken'
   return render_template("register.html")
 
-# Api individual coin stat route
+
+"""Coin list page info via CMC API"""
 @app.route('/coin_list/<username>')
 def coin_list(username):
   
   return render_template("coin_list.html", data=data, global_data=global_data)
 
-#Create a purchase or holding of crypto
+"""create a holding of crypto"""
 @app.route('/create_a_bag')
 def create_a_bag():
   _id = ObjectId()
   return render_template("create_a_bag.html", data=data, _id=_id)
 
-#Add the purchase/holding to my bags function
+"""push holding to db"""
 @app.route('/add_to_bagz/<username>', methods=["POST"])
 def add_to_bagz(username):
   user = mongo.db.user.update_one({'name': session['username']},
@@ -79,14 +77,14 @@ def add_to_bagz(username):
   
   return redirect(url_for('get_my_bagz', username = session['username']))
 
-#display/read users puchases/holdings
+"""display/read users puchases/holdings"""
 @app.route('/get_my_bagz/<username>')
 def get_my_bagz(username):
   user = mongo.db.user.find_one({'name': session['username']})
  
   return render_template('my_bagz.html', positions=user['positions'], data=data, username = session['username'])
 
-#update users puchases/holdings
+"""update users puchases/holdings"""
 @app.route('/edit_bag/<username>/<bag_id>')
 def edit_bag(username, bag_id):
   user = mongo.db.user.find_one({'name': session['username']})
@@ -94,10 +92,10 @@ def edit_bag(username, bag_id):
   for pos in positions:
     if pos['_id'] == bag_id:
       bag = pos
-  # the_bag = mongo.db.user.find({"positions":{"_id": ObjectId(bag_id)}})
+  
   return render_template("edit_bag.html", data=data, username = session['username'], bag_id=bag_id, bag=bag)
 
-#update users puchases/holdings
+"""update users puchases/holdings"""
 @app.route('/update_bag/<username>/<bag_id>', methods=["POST"])
 def update_bag(username, bag_id):
   user = mongo.db.user.update_many({'name': session['username'], 'positions._id': bag_id }, 
@@ -111,7 +109,7 @@ def update_bag(username, bag_id):
   
   return redirect(url_for('get_my_bagz', username = session['username']))
 
-#delete users puchases/holdings
+"""delete users puchases/holdings"""
 @app.route('/delete_bag/<username>/<bag_id>')
 def delete_bag(username, bag_id):
   user = mongo.db.user.update_one({'name': session['username']},
